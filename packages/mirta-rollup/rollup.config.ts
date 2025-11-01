b6ebe6998df451d80221691dcb6a3c72e1208f19
Wired Home <212203636+wihome-dev@users.noreply.github.com>
2025-11-01 16:35:53 +0300

    refactor(configs): simplify rollup configs usage (#113)

--- packages/mirta-rollup/rollup.config.ts
+++ packages/mirta-rollup/rollup.config.ts
@@ -3,7 +3,8 @@ import { definePackageConfig } from '#src/index'
 export default definePackageConfig({
   input: [
     'src/index.ts',
-    'src/configs/index.ts',
+    'src/config.ts',
+    'src/config-package.ts',
     'src/utils/env-loader.ts',
   ],
 })
---
+++ packages/mirta-rollup/src/config.ts
@@ -0,0 +1,10 @@
+import { defineRuntimeConfig } from '#configs/runtime'
+
+/**
+ * Конфигурация Rollup по умолчанию, предназначенная
+ * для сборки под контроллер Wiren Board.
+ *
+ * @since 0.4.0
+ *
+ **/
+export default defineRuntimeConfig()
